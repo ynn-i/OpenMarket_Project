b1a26cb2b67add507a7a6ba9a142e69942da6301
진승현 <149146164+ynn-i@users.noreply.github.com>
2024-08-06 10:23:00 +0900

    메인페이지 기존 상품 불러오기 기능 추가

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+// Create Ui
+const createProductUi = (productData) => {
+    const productGallery = document.querySelector('.product-gallery');
+    const productCon = document.createElement('div');
+    productCon.classList.add('product-con');
+    const productThumb = document.createElement('div');
+    productThumb.classList.add('product-thumb');
+    const productImage = document.createElement('img');
+    productImage.src = productData.image;
+    productImage.alt = productData.product_name;
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    const productSubTitle = document.createElement('p');
+    productSubTitle.classList.add('sub-title');
+    productSubTitle.textContent = `${productData.store_name}`;
+    const productMainTitle = document.createElement('p');
+    productMainTitle.classList.add('main-title');
+    productMainTitle.textContent = productData.product_name;
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('product-price');
+    productPrice.textContent = `${productData.price}`;
+    const productPriceUnit = document.createElement('span');
+    productPriceUnit.classList.add('product-price-unit');
+
+    // $todoText.textContent = todoData.todo;
+    productGallery.appendChild(productCon);
+    productCon.appendChild(productThumb);
+    productThumb.appendChild(productImage);
+    productCon.appendChild(productInfo);
+    productInfo.appendChild(productSubTitle);
+    productInfo.appendChild(productMainTitle);
+    productInfo.appendChild(productPrice);
+    productPrice.appendChild(productPriceUnit);
+};
+
+const getProducts = async () => {
+    const res = await fetch('https://openmarket.weniv.co.kr/products/');
+    const productDatas = await res.json();
+    return productDatas;
+};
+const initProducts = async () => {
+    const productDatas = await getProducts();
+    if (productDatas && productDatas.results) {
+        productDatas.results.forEach((productData) => {
+            createProductUi(productData);
+        });
+    } else {
+        console.error('상품이 존재하지 않습니다.');
+    }
+};
+initProducts();
