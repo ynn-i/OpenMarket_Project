b038ea7a85495ad3d43d7dd34830d1eb5f02d166
진승현 <149146164+ynn-i@users.noreply.github.com>
2024-08-08 11:23:26 +0900

    로그인 로직 수정

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,10 @@
 // 아이디 비밀번호 검증 및 로그인
-async function login(id, pw, login_type) {
-    const loginType = document.getElementById('login-tab').checked;
+async function login() {
+    const loginType = document.querySelector('input[name="tabs"]:checked')?.id === 'login-tab' ? 'BUYER' : 'SELLER';
     const ID = document.getElementById('id').value.trim();
     const PW = document.getElementById('pw').value.trim();
     const error = document.querySelector('.error');
+
     let errorMessage = '';
 
     if (ID === '' && PW === '') {
@@ -13,9 +14,11 @@ async function login(id, pw, login_type) {
     } else if (PW === '') {
         errorMessage = '비밀번호를 입력해 주세요.';
     }
+
     if (errorMessage) {
         error.textContent = errorMessage;
-        error.style.display = 'block';
+        error.style.setProperty('display', 'block', 'important');
+
         return;
     }
 
@@ -24,16 +27,16 @@ async function login(id, pw, login_type) {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                username: id,
-                password: pw,
-                login_type: loginType ? 'SELLER' : 'BUYER',
+                username: ID,
+                password: PW,
+                login_type: loginType,
             }),
         });
         const data = await req.json();
 
         if (data.success) {
-            alert(loginData.login_type === 'BUYER' ? '구매회원 로그인 성공' : '판매회원 로그인 성공');
             window.location.href = loginData.login_type === 'BUYER' ? '/index.html' : '/index.html';
+            localStorage.setItem('token', data.token);
         } else {
             errorMessage = '아이디 또는 비밀번호가 일치하지 않습니다.';
         }
@@ -41,6 +44,7 @@ async function login(id, pw, login_type) {
         alert('로그인에 실패하였습니다. 다시 시도해주세요.');
     }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.login-btn').addEventListener('click', login);
 });
