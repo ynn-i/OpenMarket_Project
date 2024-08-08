7931dafd8845f834d71b64d2efcdaf4d05b30967
진승현 <149146164+ynn-i@users.noreply.github.com>
2024-08-08 14:18:53 +0900

    test

--- scripts/index.js
+++ scripts/index.js
@@ -69,15 +69,21 @@ function closeModal() {
 function handleLoginModal(event) {
     const token = localStorage.getItem('token');
     if (token) {
-        alert('Redirecting to cart page');
-        window.location.href = '/cart.html';
+        window.location.href = 'https://ynn-i.github.io/OpenMarket_Project/cart.html';
     } else {
         openModal(event);
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    openLoginBtn.addEventListener('click', handleLoginModal);
+    // openLoginBtn.addEventListener('click', handleLoginModal);
+
+    if (openLoginBtn) {
+        openLoginBtn.addEventListener('click', handleLoginModal);
+        console.log('이벤트 추가');
+    } else {
+        console.log('연결X');
+    }
 
     closeLoginBtn.addEventListener('click', closeModal);
     noLoginBtn.addEventListener('click', closeModal);
