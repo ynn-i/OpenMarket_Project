b1dd4afdd1632571ae013a9f91ba7467524c92de
진승현 <149146164+ynn-i@users.noreply.github.com>
2024-08-12 17:43:54 +0900

    장바구니 UI, js 작업

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,36 +1,67 @@
-const createCarttUi = (productData) => {};
+const createCartUi = (productData) => {
+    document.body.appendChild(cartListCon);
+    const cartListCon = document.createElement('ul');
+    cartListCon.classList.add('cart-list');
+    const cartList = document.createElement('li');
+    const checkCon = document.createElement('div');
+    checkCon.classList.add('check');
+    const checkLabel = document.createElement('label');
+    checkLabel.querySelector.add('check-cart');
+    const checkInp = document.createElement('input');
+    checkInp.type = 'checkbox';
+    const productCart = document.createElement('div');
+    productCart.classList.add('product-cart');
+    const productThumbCart = document.createElement('img');
+    productThumbCart.classList.add('product-thumb-cart');
+    const infoCon = document.createElement('div');
+    infoCon.classList.add('info-con');
+    const subTitle = document.createElement('p');
+    subTitle.classList.add('sub-title');
+    subTitle.textContent = productData.subTitle;
+    const mainTitle = document.createElement('p');
+    mainTitle.classList.add('main-title');
+    mainTitle.textContent = productData.product_name;
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('product-price');
+    productPrice.textContent = `${productData.price}원`;
+    const delivery = document.createElement('p');
+    delivery.classList.add('delivery');
+    delivery.textContent = productData.shipping_method;
+
+    cartListCon.appendChild(cartList);
+    cartList.appendChild(checkCon);
+    checkCon.appendChild(checkLabel);
+    checkCon.appendChild(checkInp);
+    cartList.appendChild(productCart);
+    productCart.appendChild(productThumbCart);
+    productCart.appendChild(infoCon);
+    infoCon.appendChild(subTitle);
+    infoCon.appendChild(mainTitle);
+    infoCon.appendChild(productPrice);
+    infoCon.appendChild(delivery);
+};
+
+const getToken = () => localStorage.getItem('login-token');
 
 const getCart = async () => {
-    try {
-        const req = await fetch('https://openmarket.weniv.co.kr/cart/', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        const data = await req.json();
-        console.log(data);
-
-        if (data.count > 0) {
-            return data;
-        } else {
-            return null;
-        }
-    } catch (error) {
-        alert('로딩에 실패하였습니다. 다시 시도해주세요.');
-        return null;
-    }
+    const res = await fetch('https://openmarket.weniv.co.kr/cart/', {
+        method: 'GET',
+        headers: {
+            Authorization: `JWT ${getToken()}`,
+        },
+    });
+    const productDatas = await res.json();
+    return productDatas;
 };
 
 const initCart = async () => {
-    const cartDatas = await getCart();
-    if (cartDatas && cartDatas.results) {
-        cartDatas.results.forEach((productData) => {
+    const productDatas = await getCart();
+    if (productDatas && productDatas.results) {
+        productDatas.results.forEach((productData) => {
             createCartUi(productData);
         });
     } else {
-        console.error('장바구니에 상품이 존재하지 않습니다.');
+        console.error('상품이 존재하지 않습니다.');
     }
 };
 initCart();
